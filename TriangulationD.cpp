a4911527407612078ac2e4a2ff0cdb6cb2e82cad
deadh8er <56890932+deadh8er@users.noreply.github.com>
2021-03-08 15:36:24 +0300

    Initial commit

---
+++ TriangulationD.cpp
@@ -0,0 +1,12 @@
+﻿// TriangulationD.cpp: определяет точку входа для приложения.
+//
+
+#include "TriangulationD.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
