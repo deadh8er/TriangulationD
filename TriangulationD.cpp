c0bc447d57b0c6cbcb2db55d6a46d003b4c2c461
deadh8er <56890932+deadh8er@users.noreply.github.com>
2021-04-19 21:15:56 +0300

    argc,argv

--- TriangulationD.cpp
+++ TriangulationD.cpp
@@ -4,15 +4,34 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "TriangulationD.h"
 
+char* logfile;
+
+enum Result
+{
+    SUCCESS = 0,
+    ERROR_OPENING_FILE = -1,
+    ERROR_READING_FILE = -2,
+    ERROR_NOT_SPECIFIED_FILE = -3
+};
+
 using namespace std;
 
-int readflie(int M, vector<vector<float> >& triangle) {
+void writelog(char* message) {
+    ofstream out(logfile, std::ios::app);
+    out << message << endl;
+    out.close();
+}
+
+int readflie(char* inputfile, int M, vector<vector<float> >& triangle, int argc) {
     vector <float> strf1(M); //вектор для одной точки
     string s;
     int circuit = 0;
     int i = 0;
-    ifstream cin("read.dat");
-    if (!cin) { printf("Can't open file read.dat\n Terminate\n"); exit(-1); }
+    ifstream cin(inputfile);
+    if (!cin) {
+        if (argc == 4) writelog("Can't open input file\nTerminate");
+        printf("Can't open input file\nTerminate\n"); exit(ERROR_OPENING_FILE);
+    }
 
     while (getline(cin, s)) {
         if (s == "EXTERNAL CIRCUIT") {
@@ -31,8 +50,8 @@ int readflie(int M, vector<vector<float> >& triangle) {
     return i;
 }
 
-void writefile(int N1, int N2, int M, vector<vector<float> >& triangle) {
-    ofstream out("write.vtu");
+void writefile(char* outputfile, int N1, int N2, int M, vector<vector<float> >& triangle) {
+    ofstream out(outputfile);
     out << "# vtk DataFile Version 2.0";
     out << endl << "TriangulationD result";
     out << endl << "ASCII";
@@ -55,14 +74,36 @@ void writefile(int N1, int N2, int M, vector<vector<float> >& triangle) {
     out.close();
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     vector < vector < float > > triangle; //triangle - двумерный вектор входных точек
     int N1, N2, M = 2; //N1 - количество строк точек внешнего контура, N2 - количество строк всех точек в файле (только один внутренний контур), M - столбцов в triangle
 
-    N1 = readflie(M, triangle); //читает файл и записывает в triangle, возврашает число строк
+    if (argc == 1) {
+        printf("The input file is undefined\nTerminate\n"); exit(ERROR_NOT_SPECIFIED_FILE);
+    }
+
+    if (argc == 4) {
+        logfile = argv[3];
+
+        ofstream out(logfile); out.close();
+
+        writelog("The log file is defined");
+        printf("The log file is defined\n");
+    }
+
+    char* inputfile = argv[1];
+    N1 = readflie(inputfile, M, triangle, argc); //читает файл и записывает в triangle, возврашает число строк
+    if (argc == 4) writelog("Mesh loaded into memory");
+    printf("Mesh loaded into memory\n");
     N2 = triangle.size();
-    writefile(N1, N2, M, triangle);
+
+    if (argc > 2) {
+        char* outputfile = argv[2];
+        writefile(outputfile, N1, N2, M, triangle); //записывает в файл полученную сетку
+        if (argc == 4) writelog("Mesh is written to the output file");
+        printf("Mesh is written to the output file\n");
+    }
 
     //for (int i = 0; i < N2; i++) {
     //    for (int j = 0; j < M; j++) {
