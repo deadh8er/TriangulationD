8b9de9e69f0e8e48fe26c1630700b6103ccd4738
deadh8er <56890932+deadh8er@users.noreply.github.com>
2021-05-12 13:56:38 +0300

    TriangulationD

--- TriangulationD.cpp
+++ TriangulationD.cpp
@@ -8,13 +8,13 @@ char *logfile, *inputfile, *outputfile;
 
 struct Nodes
 {
-    double x; //координата X
-    double y; //координата Y
+    double x; // Координата X
+    double y; // Координата Y
 };
 struct Triangles
 {
-    int nodes[3]; //узлы треугольника
-    int triangles[3]; //сосоедние треугольники
+    int nodes[3]; // Узлы треугольника
+    int triangles[3]; // Соседние треугольники (если значение равно -1, то соседа нет)
 };
 
 
@@ -28,12 +28,16 @@ enum Result
 
 using namespace std;
 
+
+// Запись лога
 void writelog(char* message) {
     ofstream out(logfile, std::ios::app);
     out << message << endl;
     out.close();
 }
 
+
+// Читает файл и записывает точки в AllDataNodes, возвращает количество точек
 int readflie(int M, vector<Nodes>& AllDataNodes, int argc) {
     string s;
     int circuit = 0;
@@ -62,7 +66,9 @@ int readflie(int M, vector<Nodes>& AllDataNodes, int argc) {
     return i;
 }
 
-void writefile(int N, int M, vector<Nodes>& AllDataNodes, vector<Triangles> AllDataTriangles) {
+
+// Записывает в файл полученную сетку
+void writefile(int N, int M, vector<Nodes>& AllDataNodes, vector<Triangles>& AllDataTriangles) {
     ofstream out(outputfile);
     out << "# vtk DataFile Version 2.0";
     out << endl << "TriangulationD result";
@@ -72,21 +78,28 @@ void writefile(int N, int M, vector<Nodes>& AllDataNodes, vector<Triangles> AllD
     for (int i = 0; i < N; i++) {
         out << AllDataNodes[i].x << " " << AllDataNodes[i].y << " " << 0 << endl;
     }
+    out << endl << "LINES " << AllDataTriangles.size() << " " << AllDataTriangles.size() * 4 << endl;
+    for (int i = 0; i < AllDataTriangles.size(); i++) {
+        out << AllDataTriangles[i].nodes[0] << " " << AllDataTriangles[i].nodes[1] << " " << AllDataTriangles[i].nodes[2] << " " << AllDataTriangles[i].nodes[0] << endl;
+    }
     out.close();
 }
 
+
+// Какая из точек лежит дальше относительно центра?
 bool dis(Nodes a, Nodes b) {
     return sqrt(pow(a.x, 2) + pow(a.y, 2)) < sqrt(pow(b.x, 2) + pow(b.y, 2));
 }
 
+// Задание суперструктуры - треугольник
 Triangles SuperstructureSet(vector<Nodes>& Superstructure) {
     Triangles triSuper;
     Nodes triPoint;
     int N = Superstructure.size();
 
-    sort(Superstructure.begin(), Superstructure.end(), &dis); //сортируем по удалённости
+    sort(Superstructure.begin(), Superstructure.end(), &dis); // Сортируем по удалённости
 
-    vector<Nodes> maxPoint(4); //квадрат, покрывающий все точки 
+    vector<Nodes> maxPoint(4); // Квадрат, покрывающий все точки 
 
     maxPoint[0].x = abs(Superstructure[N - 1].x);
     maxPoint[0].y = abs(Superstructure[N - 1].y);
@@ -96,9 +109,10 @@ Triangles SuperstructureSet(vector<Nodes>& Superstructure) {
     maxPoint[2].y = -abs(Superstructure[N - 1].y);
     maxPoint[3].x = -abs(Superstructure[N - 1].x);
     maxPoint[3].y = abs(Superstructure[N - 1].y);
+    
+    double ribLength = sqrt(pow(maxPoint[0].x - maxPoint[1].x, 2) + pow(maxPoint[0].y - maxPoint[1].y, 2)); // Длина ребра квадрата
 
-    double ribLength = sqrt(pow(maxPoint[0].x - maxPoint[1].x, 2) + pow(maxPoint[0].y - maxPoint[1].y, 2));
-
+    // Треугольник по вписанной окружности, описанной для квадрата
     triPoint.x = ribLength * (1 - 3 * sqrt(2)) / 2;
     triPoint.y = ribLength * (1 - 3 ) / 2;
     Superstructure.push_back(triPoint);
@@ -112,15 +126,70 @@ Triangles SuperstructureSet(vector<Nodes>& Superstructure) {
     Superstructure.push_back(triPoint);
 
     for (int i = 0; i < 3; i++) {
-        triSuper.triangles[i] = 0;
+        triSuper.triangles[i] = -1;
         triSuper.nodes[i] = N + i;
     }
 
-    return triSuper;
+    return triSuper; // Суперструктура
 }
 
-void TriangulationD(int N, int M, vector<Nodes>& AllDataNodes, vector<Triangles> AllDataTriangles) {
-    Nodes midPoint = { 0,0 }; //средняя точка
+// Вычисляет положение точки D относительно прямой AB
+double g(Nodes a, Nodes b, Nodes d) {
+    return (d.x - a.x) * (b.y - a.y) - (d.y - a.y) * (b.x - a.x);
+}
+
+// Лежат ли точки C и D с одной стороны прямой (AB)?
+bool f(Nodes a, Nodes b, Nodes c, Nodes d) {
+    return g(a, b, c) * g(a, b, d) >= 0;
+}
+
+
+// Записывает соседние треугольники для треугольника k
+void ComparisonTriangles(int k, vector<Triangles>& AllDataTriangles) {
+    AllDataTriangles[k].triangles[0] = -1;
+    AllDataTriangles[k].triangles[1] = -1;
+    AllDataTriangles[k].triangles[2] = -1;
+    int counter[4] = { 0,1,2,0 };
+    for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < AllDataTriangles.size(); i++) {
+            for (int n = 0; n < 3; n++) {
+                if ((AllDataTriangles[k].nodes[counter[j]] == AllDataTriangles[i].nodes[counter[n]] && AllDataTriangles[k].nodes[counter[j + 1]] == AllDataTriangles[i].nodes[counter[n + 1]]) || (AllDataTriangles[k].nodes[counter[j + 1]] == AllDataTriangles[i].nodes[counter[n]] && AllDataTriangles[k].nodes[counter[j]] == AllDataTriangles[i].nodes[counter[n + 1]])) {
+                    if (i == k) break;
+                    AllDataTriangles[k].triangles[j] = i;
+                }
+            }
+        }
+    }
+}
+
+// Разбиение треугольника на три. m - номер добавляемой точки, k - номер треугольника, которому принадлежит точка.
+void NewTriangles(int m, int k, vector<Nodes>& AllDataNodes, vector<Triangles>& AllDataTriangles) {
+    Triangles triangLine;
+    triangLine.nodes[0] = AllDataTriangles[k].nodes[0];
+    triangLine.nodes[1] = AllDataTriangles[k].nodes[1];
+    triangLine.nodes[2] = m;
+    AllDataTriangles.push_back(triangLine);
+
+    triangLine.nodes[0] = AllDataTriangles[k].nodes[0];
+    triangLine.nodes[1] = m;
+    triangLine.nodes[2] = AllDataTriangles[k].nodes[2];
+    AllDataTriangles.push_back(triangLine);
+
+    triangLine.nodes[0] = m;
+    triangLine.nodes[1] = AllDataTriangles[k].nodes[1];
+    triangLine.nodes[2] = AllDataTriangles[k].nodes[2];
+    AllDataTriangles.push_back(triangLine);
+
+    AllDataTriangles.erase(AllDataTriangles.begin() + k);
+
+    for (int i = AllDataTriangles.size() - 1; i >= 0; i--) {
+        ComparisonTriangles(i, AllDataTriangles);
+    }
+}
+
+// Построение триангуляции алгоритмом «Удаляй и строй» 
+void TriangulationD(int N, int M, vector<Nodes>& AllDataNodes, vector<Triangles>& AllDataTriangles) {
+    Nodes midPoint = { 0,0 }; // Средняя точка
     for (int i = 0; i < N; i++) {
         midPoint.x += AllDataNodes[i].x;
         midPoint.y += AllDataNodes[i].y;
@@ -128,27 +197,71 @@ void TriangulationD(int N, int M, vector<Nodes>& AllDataNodes, vector<Triangles>
     midPoint.x = midPoint.x / N;
     midPoint.y = midPoint.y / N;
 
+    // Смещение системы координат
     for (int i = 0; i < N; i++) {
         AllDataNodes[i].x -= midPoint.x;
         AllDataNodes[i].y -= midPoint.y;
-    } //новая система координат
+    }
 
     Triangles triangLine;
-    triangLine = SuperstructureSet(AllDataNodes); //задание суперстуктуры - треугольник
+    triangLine = SuperstructureSet(AllDataNodes); // Задание суперстуктуры - треугольник
     AllDataTriangles.push_back(triangLine);
 
+    for (int i = 0; i < N; i++) {
+        if (i != 0) {
+            bool belongPoint = false; // Принадлежит ли точка треугольнику?
 
+            for (int j = 0; j < AllDataTriangles.size(); j++) {
+                Nodes pt1 = AllDataNodes[AllDataTriangles[j].nodes[0]];
+                Nodes pt2 = AllDataNodes[AllDataTriangles[j].nodes[1]];
+                Nodes pt3 = AllDataNodes[AllDataTriangles[j].nodes[2]];
+                belongPoint = f(pt1, pt2, pt3, AllDataNodes[i]) && f(pt2, pt3, pt1, AllDataNodes[i]) && f(pt3, pt1, pt2, AllDataNodes[i]);
+                if (belongPoint) {
+                    NewTriangles(i, j, AllDataNodes, AllDataTriangles);
+                    break;
+                }
+            }
+        }
+        else {
+            triangLine.nodes[0] = AllDataTriangles[0].nodes[0];
+            triangLine.nodes[1] = AllDataTriangles[0].nodes[1];
+            triangLine.nodes[2] = 0;
+            triangLine.triangles[0] = -1;
+            triangLine.triangles[1] = 1;
+            triangLine.triangles[2] = 2;
+            AllDataTriangles.push_back(triangLine);
+
+            triangLine.nodes[0] = AllDataTriangles[0].nodes[0];
+            triangLine.nodes[1] = 0;
+            triangLine.nodes[2] = AllDataTriangles[0].nodes[2];
+            triangLine.triangles[0] = 0;
+            triangLine.triangles[1] = -1;
+            triangLine.triangles[2] = 2;
+            AllDataTriangles.push_back(triangLine);
+
+            triangLine.nodes[0] = 0;
+            triangLine.nodes[1] = AllDataTriangles[0].nodes[1];
+            triangLine.nodes[2] = AllDataTriangles[0].nodes[2];
+            triangLine.triangles[0] = 0;
+            triangLine.triangles[1] = 1;
+            triangLine.triangles[2] = -1;
+            AllDataTriangles.push_back(triangLine);
+
+            AllDataTriangles.erase(AllDataTriangles.begin()); // Удаление старого треугольника
+        }
+
+    }
 }
 
 int main(int argc, char* argv[])
 {
     vector<Nodes> AllDataNodes;
     vector<Triangles> AllDataTriangles;
-    int N, M = 2; //N - количкество точек
+    int N, M = 2; // N - количество точек в файле
 
-    //if (argc == 1) {
-    //    printf("The input file is undefined\nTerminate\n"); exit(ERROR_NOT_SPECIFIED_FILE);
-    //}
+    if (argc == 1) {
+        printf("The input file is undefined\nTerminate\n"); exit(ERROR_NOT_SPECIFIED_FILE);
+    }
 
     if (argc == 4) {
         logfile = argv[3];
@@ -159,19 +272,19 @@ int main(int argc, char* argv[])
         printf("The log file is defined\n");
     }
 
-    //inputfile = argv[1];
-    inputfile = "read.dat";
-    N = readflie(M, AllDataNodes, argc); //читает файл и записывает в AllDataNodes, возврашает число точек внешнего конутра
+    inputfile = argv[1];
+    //inputfile = "read.dat";
+    N = readflie(M, AllDataNodes, argc);
     if (argc == 4) writelog("Mesh loaded into memory");
     printf("Mesh loaded into memory\n");
-    N = AllDataNodes.size(); //общее число точек
+    N = AllDataNodes.size();
 
     TriangulationD(N, M, AllDataNodes, AllDataTriangles);
 
     if (argc > 2) {
         outputfile = argv[2];
         N = AllDataNodes.size();
-        writefile(N, M, AllDataNodes, AllDataTriangles); //записывает в файл полученную сетку
+        writefile(N, M, AllDataNodes, AllDataTriangles);
         if (argc == 4) writelog("Mesh is written to the output file");
         printf("Mesh is written to the output file\n");
     }
